9d7d49f1e80f28a41910765835a089c24cf83760
Malindrie Dharmaratne <s4566105@uq.edu.au>
2021-02-09 13:24:52 +1000

    First commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' My Hello World Function
+#'
+#' @param x The name of the person to say hi to
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("John")
+#' \dontrun{
+#' hello("Steve")
+#' }
+hello <- function(x) {
+  print(paste0("Hello ", x, ", this is the world!"))
+}
---
+++ R/version.R
@@ -0,0 +1,16 @@
+#' A Statistical Framework for Modeling and Identifying Differential Distributions in Single-cell RNA-sequencing Data
+#'
+#' @export
+#' @return ... Reports the version of the package:scShapes.
+#
+version <- function() {
+  cat("           ____\n")
+  cat("          |     |\n")
+  cat("____ ____ |____ |____ ____ ____ ____ ____\n")
+  cat("[__  |        | |   | ___| |   ||___] [__\n")
+  cat("___] |___ ____| |   ||___| |___||___  ___]\n")
+  cat("                           |\n")
+  cat("                           |\n")
+  cat("                            Ver:0.1.0\n")
+  cat("\n")
+}
