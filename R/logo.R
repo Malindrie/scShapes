e5199c0a6e07799284bf2f3867c514949e232101
Malindrie Dharmaratne <s4566105@uq.edu.au>
2021-06-02 13:25:30 +1000

    Changed version to logo

---
+++ R/logo.R
@@ -0,0 +1,15 @@
+#' A Statistical Framework for Modeling and Identifying Differential Distributions in Single-cell RNA-sequencing Data
+#'
+#' @export
+#' @return ... Reports the logo of the package:scShapes.
+#
+logo <- function() {
+  cat("            ____\n")
+  cat("          |     |\n")
+  cat("____ ____ |____ |____ ____ ____ ____  ____\n")
+  cat("[__  |        | |   | ___| |   ||___] [__\n")
+  cat("___] |___ ____| |   ||___| |___||___  ___]\n")
+  cat("                           |\n")
+  cat("                           |\n")
+  cat("\n")
+}
---
+++ tests/testthat/test-logo.R
@@ -0,0 +1,3 @@
+test_that("Show the logo", {
+  logo()
+})
